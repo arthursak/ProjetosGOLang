25e257403514f4e4e59d28c15888eef2dc241d54
Arthur Murilo da Rocha <arthurmourilo@gmail.com>
2022-10-27 22:55:37 -0300

    Finalizando aula 4

--- src/banco/contas/contaCorrente.go
+++ src/banco/contas/contaCorrente.go
@@ -1,27 +1,31 @@
 package contas
 
+import (
+	"banco/clientes"
+)
+
 type ContaCorrente struct {
-	Titular string
+	Titular clientes.Titular
 	Agencia int
 	Conta   int
-	Saldo   float64
+	saldo   float64
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 
 	if podeSacar {
-		c.Saldo -= valorDoSaque
-		return "Saque realizado com sucesso!\nSaldo: R$", c.Saldo
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso!\nsaldo: R$", c.saldo
 	} else {
-		return "Saldo insuficiente! Saldo:\nR$", c.Saldo
+		return "saldo insuficiente! saldo:\nR$", c.saldo
 	}
 }
 
 func (c *ContaCorrente) Transferir(valorDaTransf float64, contaDestino *ContaCorrente) string {
 
-	if valorDaTransf < c.Saldo && valorDaTransf > 0 {
-		c.Saldo -= valorDaTransf
+	if valorDaTransf < c.saldo && valorDaTransf > 0 {
+		c.saldo -= valorDaTransf
 		contaDestino.Depositar(valorDaTransf)
 		return "Transferencia efetuada com sucesso!"
 	} else {
@@ -32,10 +36,14 @@ func (c *ContaCorrente) Transferir(valorDaTransf float64, contaDestino *ContaCor
 func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
 
 	if valorDeposito > 0 {
-		c.Saldo += valorDeposito
+		c.saldo += valorDeposito
 	} else {
-		return "Não é possivel depositar valores negativos!\nSaldo: R$", c.Saldo
+		return "Não é possivel depositar valores negativos!\nsaldo: R$", c.saldo
 	}
 
-	return "Deposito feito com sucesso! Saldo:\nR$", c.Saldo
+	return "Deposito feito com sucesso! saldo:\nR$", c.saldo
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
 }
--- src/banco/main.go
+++ src/banco/main.go
@@ -6,16 +6,9 @@ import (
 )
 
 func main() {
-	contaDoTuts := contas.ContaCorrente{Titular: "Arthur", Saldo: 200.50}
-	fmt.Println(contaDoTuts)
+	contaTuts := contas.ContaCorrente{}
+	contaTuts.Depositar(100)
 
-	contaDaFran := contas.ContaCorrente{Titular: "Francine", Saldo: 200.50}
-	fmt.Println(contaDaFran)
-
-	status := contaDoTuts.Transferir(100., &contaDaFran)
-
-	fmt.Println(status)
-	fmt.Println(contaDoTuts)
-	fmt.Println(contaDaFran)
+	fmt.Println(contaTuts.ObterSaldo())
 
 }
